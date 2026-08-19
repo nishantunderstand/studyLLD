b745e1e0f13208db5b1782943b38e88c067f7a7d
Nishant Raj <nishant.read@gmail.com>
2026-08-20 00:46:12 +0530

    LenovoBat 20-Aug-26  0:46:12.67

---
+++ src/main/java/_4a_SOLID_SingleResponsibility/PaymentService.java
@@ -0,0 +1,11 @@
+package _4a_SOLID_SingleResponse;
+
+
+//S — Single Responsibility Principle
+//A class should have one responsibility / one reason to change.
+
+class PaymentService {
+    void processPayment() {}
+    void saveToDB() {}
+    void sendEmail() {}
+}
